b20ec8b95c3a7bfc78c08b073c3f0a86088596d9
JoenanBurgundy <joenannr@gmail.com>
2020-05-19 12:33:35 +0700

    [FIX] onchange operating

--- sale_order_operating_unit/models/sale_order.py
+++ sale_order_operating_unit/models/sale_order.py
@@ -1,5 +1,6 @@
 from odoo import models, fields, api, _
-from odoo.exceptions import UserError
+from odoo.exceptions import UserError, ValidationError
+
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
@@ -14,9 +15,9 @@ class SaleOrder(models.Model):
     #     types = type_obj.search([('code', '=', 'incoming'),
     #                              ('warehouse_id.operating_unit_id', '=',
     #                               operating_unit.id)])
-        
+
     #     return types[:1].id
-        # return
+    # return
 
     READONLY_STATES = {
         'sale': [('readonly', True)],
@@ -31,7 +32,7 @@ class SaleOrder(models.Model):
         required=True,
         default=lambda self: (self.env['res.users'].
                               operating_unit_default_get(self.env.uid))
-    )    
+    )
 
     # picking_type_id = fields.Many2one(
     #     comodel_name='stock.picking.type',
@@ -73,18 +74,12 @@ class SaleOrder(models.Model):
 
     @api.onchange('operating_unit_id')
     def _onchange_operating_unit_id(self):
-        type_obj = self.env['stock.picking.type']
+        warehouse_obj = self.env['stock.warehouse']
         if self.operating_unit_id:
-            types = type_obj.search([('code', '=', 'incoming'),
-                                     ('warehouse_id.operating_unit_id', '=',
-                                      self.operating_unit_id.id)])
-            if types:
-                self.picking_type_id = types[:1]
-            else:
-                raise UserError(
-                    _("No Warehouse found with the Operating Unit indicated "
-                      "in the Sale Order")
-                )
+            warehouses = warehouse_obj.search([('operating_unit_id', '=',
+                                                self.operating_unit_id.id)], limit=1)
+            if warehouses:
+                self.warehouse_id = warehouses[:1]
 
     @api.model
     def _prepare_procurement_values(self):
